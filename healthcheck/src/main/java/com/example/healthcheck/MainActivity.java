f947a1d91c27128fe212eab64c5ba904da84079e
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2021-09-16 02:46:26 +0100

    healthy living app

---
+++ healthcheck/src/main/java/com/example/healthcheck/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.healthcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int healthLevel;
+    String message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    }
+
+    public void displayMessage(String text) {
+        TextView message = (TextView) findViewById(R.id.display_message);
+        message.setText(text);
+    }
+
+    public void checkResult(View view) {
+        displayFinalResultText();
+        displayCount(healthLevel);
+    }
+
+    public void yes(View view) {
+        healthLevel++;
+        message = "You answered yes, current health level is " + healthLevel;
+        displayHealthLevelCount(healthLevel);
+        displayMessage(message);
+    }
+
+    public void sometimes(View view) {
+        healthLevel = healthLevel + 0;
+        message = "You answered sometimes, current health level is " + healthLevel;
+        displayHealthLevelCount(healthLevel);
+        displayMessage(message);
+    }
+
+    public  void no(View view) {
+        healthLevel--;
+        message = "You answered no, current health level is " + healthLevel;
+        displayHealthLevelCount(healthLevel);
+        displayMessage(message);
+    }
+
+    private void displayHealthLevelCount(int score) {
+        TextView currentScore = (TextView) findViewById(R.id.current_score);
+        currentScore.setText("Your current score is " + score);
+    }
+
+    private void displayFinalResultText() {
+        TextView finalResultText = (TextView) findViewById(R.id.result_message);
+        finalResultText.setText("Final Result:");
+    }
+
+    private void displayCount(int scored) {
+        TextView count = (TextView) findViewById(R.id.count_message);
+        count.setText("You scored " + scored );
+    }
+}
