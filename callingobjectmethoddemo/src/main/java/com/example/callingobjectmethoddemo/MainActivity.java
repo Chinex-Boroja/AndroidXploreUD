69531442dbf8e0401273d9132e2b5f1a685eb3fb
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2021-09-21 17:54:54 +0100

    calling objects on method

--- callingobjectmethoddemo/src/main/java/com/example/callingobjectmethoddemo/MainActivity.java
+++ callingobjectmethoddemo/src/main/java/com/example/callingobjectmethoddemo/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.callingobjectmethoddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**TextView textView = new TextView(this); // referring to the current Activity
+        textView.setText("We Back!!");
+        textView.setTextColor(Color.RED);
+        textView.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        textView.setTextSize(36);
+        setContentView(textView);*/
     }
 }
